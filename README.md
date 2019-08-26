c32a1617b40cd7de68158dc2ed565671b6738583
fancyzhong <fancyzhong@tencent.com>
2019-08-26 10:00:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+### how to use
+- step 1, install it
+```
+npm i concent-plugin-redux-devtool
+```
+- setp 2, config it to concent startupOption's plugins
+```
+import { run } from 'concent';
+import reduxDevToolPlugin from 'concent-plugin-redux-devtool';
+
+// your store config
+const storeConfig = {};
+
+// your option
+const startupOption = {
+  plugins: [reduxDevToolPlugin]
+};
+
+run(storeConfig, startupOption);
+```
+>
+- run your concent app, check your chrome redux dev-tool
+
+![dev-tool-pic](https://raw.githubusercontent.com/fantasticsoul/assets/master/img/cc-eco/cc-pic1.png)
+> note that `committedState` is the whole state that committed by some one of your component refs, `sharedState` is the state that will save to store, they are different!
---
+++ index.js
@@ -0,0 +1,67 @@
+var { cst, ccContext, getState } = require('concent');
+var { createStore, combineReducers } = require('redux');
+
+var pluginName = 'reduxDevTool';
+var toExport = module.exports = {};
+var reduxStore = null;
+
+function createReducer(module, initState) {
+  return function (state, action) {
+    if (state === undefined) state = initState;
+
+    if (action.module === module) {
+      return Object.assign(state, action.payload);
+    } else {
+      return state;
+    }
+  };
+}
+
+function createReducers() {
+  const modules = Object.keys(ccContext.moduleName_stateKeys_);
+  const reducers = {};
+  modules.forEach(function (m) {
+    reducers[m] = createReducer(m, getState(m));
+  });
+  return reducers;
+}
+
+function injectReduxDevTool() {
+  reduxStore = createStore(
+    combineReducers(createReducers()),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+  reduxStore.subscribe(function(){
+    if (actionLen === actionPrevLen + 1) {
+      //来自于concent的监听
+      actionPrevLen++;
+    } else {
+      // 来自于devtool点击jump 或者 skip
+      if (window.alert) window.alert('暂未打通concent与redux dev tool的jump、skip等功能');
+    }
+  });
+
+}
+
+var actionLen = 1;
+var actionPrevLen = 1;
+function dispatchAction(actionForRedux){
+  if (reduxStore) {
+    actionLen++;
+    reduxStore.dispatch(actionForRedux);
+  }
+}
+
+/** concent启动时会调用一次插件的install接口 */
+toExport.install = function (on) {
+  injectReduxDevTool();
+
+  on(cst.SIG_STATE_CHANGED, function (data) {
+    dispatchAction(data.payload)
+  });
+  return { name: pluginName }
+}
+
+
+toExport.default = toExport;
