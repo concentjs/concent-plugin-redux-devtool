d5280d37fdef38eac209832b4b44f91afe064c39
fancyzhong <fancyzhong@tencent.com>
2021-04-09 11:42:13 +0800

    reafactor: mod package types

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-type DefaultExport = {
-  install: (on: any) => { name: string };
-  setConf: (params: { log: boolean }) => void;
-};
-
-declare const toExport: DefaultExport;
-
-export default toExport;
